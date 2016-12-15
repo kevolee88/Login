a25b949bdd72d74b3b0ca8971e7ad537a970418e
Kevin Rogers <kevin.rogers@thecontrolgroup.com>
2016-12-15 14:50:15 -0800

    add client side form validation

--- source/index.php
+++ source/index.php
@@ -29,13 +29,13 @@ $assets_root  = 'http://' . $_SERVER['HTTP_HOST'] . '/login/assets';
         <div id="login-form">
           <h1>Welcome Back!</h1>
 
-          <form id="form-login" action="index.html" method="post">
+          <form id="form-login" action="" method="post" data-parsley-validate>
 
             <div class="field-wrap">
-              <input id="email_address" type="text" name="email_address" value="" placeholder="Email Address">
+              <input id="email_address" type="email" name="email_address" value="" placeholder="Email Address" required data-parsley-error-message="Please enter valid email" data-parsley-type="email">
             </div>
             <div class="field-wrap">
-              <input id="password" type="password" name="password" value="" placeholder="Password">
+              <input id="password" type="password" name="password" value="" placeholder="Password" required data-parsley-error-message="Please enter a password">
             </div>
             <button type="submit" name="button">Login</button>
 
@@ -47,24 +47,24 @@ $assets_root  = 'http://' . $_SERVER['HTTP_HOST'] . '/login/assets';
         <div id="sign-up-form">
           <h1>Sign Up for Free</h1>
 
-          <form id="form-sign-up" action="index.html" method="post">
+          <form id="form-sign-up" action="" method="post" data-parsley-validate>
 
             <div class="top-row">
               <div class="field-wrap">
-                <input id="first-name" type="text" name="first_name" value="" placeholder="First Name">
+                <input id="first-name" type="text" name="first_name" value="" placeholder="First Name" required data-parsley-error-message="Please enter your first name">
               </div>
               <div class="field-wrap">
-                <input id="last-name" type="text" name="last_name" value="" placeholder="Last Name">
+                <input id="last-name" type="text" name="last_name" value="" placeholder="Last Name" required data-parsley-error-message="Please enter your last name">
               </div>
             </div>
             <div class="field-wrap">
-              <input id="email" type="text" name="email" value="" placeholder="Email">
+              <input id="email" type="text" name="email" value="" placeholder="Email" required data-parsley-error-message="Please enter valid email" data-parsley-type="email">
             </div>
             <div class="field-wrap">
-              <input id="username" type="text" name="username" value="" placeholder="Username">
+              <input id="username" type="text" name="username" value="" placeholder="Username" required data-parsley-error-message="Please enter username">
             </div>
             <div class="field-wrap">
-              <input id="password" type="password" name="password" value="" placeholder="Password">
+              <input id="password" type="password" name="password" value="" placeholder="Password" required data-parsley-error-message="Please enter a password">
             </div>
             <button type="submit" name="button">Get Started</button>
 
@@ -78,7 +78,9 @@ $assets_root  = 'http://' . $_SERVER['HTTP_HOST'] . '/login/assets';
     </section>
 
     <script src="http://code.jquery.com/jquery-3.1.1.min.js" ></script>
-    <script type="text/javascript" src="<?=$assets_root;?>/js/script.js"></script>
+    <script src="<?=$assets_root;?>/plugins/parsley-v2.6.0.js" ></script>
+    <script src="<?=$assets_root;?>/plugins/serializejson-v2.8.1.js" ></script>
+    <script src="<?=$assets_root;?>/js/script.js"></script>
 
     <script>
       window.uri= '<?php echo $uri; ?>';
